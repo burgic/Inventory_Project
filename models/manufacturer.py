58eda3b67606abd60f704acd46246f5e9d5c4575
burgic <ceburgin@gmail.com>
2022-03-20 20:59:53 +0100

    1st, strucutre set up

---
+++ models/manufacturer.py
@@ -0,0 +1,7 @@
+class Manufacturer:
+    def __init__(self, name, location, payment_days, id):
+        self.name = name
+        self.location = location
+        self.payment_days = payment_days
+        self.id = id
+        
