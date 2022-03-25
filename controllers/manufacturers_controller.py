1e5c92a653a79e72271f80174b3e984d4ab06075
burgic <ceburgin@gmail.com>
2022-03-25 14:20:34 +0000

    new manufacturer

--- controllers/manufacturers_controller.py
+++ controllers/manufacturers_controller.py
@@ -4,23 +4,25 @@ from models.manufacturer import Manufacturer
 from repositories import manufacturer_repository
 from repositories import product_repository
 from models.product import Product
+from models.manufacturer import Manufacturer
 
-manufacturer_blueprint = Blueprint("tasks", __name__)
+# manufacturer_blueprint = Blueprint("tasks", __name__)
 
 
-@manufacturer_blueprint.route("/new_manufacturer")
-def new_manufacturer():
-    manufacturers = manufacturer_repository.select_all()
-    return render_template("/new_manufacturer.html", title="New Manufacturer", manufacturers = manufacturers)
+# @manufacturer_blueprint.route("/new_manufacturer.html")
+# def new_manufacturer():
+#     manufacturers = manufacturer_repository.select_all()
+#     products = product_repository.select_all()
+#     return render_template("/new_manufacturer.html", title="New Manufacturer", manufacturers = manufacturers)
 
-@manufacturer_blueprint.route("/new_manufacturer")
-def create_manufacturer():
-    name = request.form['name']
-    location = request.form['location']
-    payment_days = request.form['payment_days']
-    payment_code = request.form['payment_code']
+# @manufacturer_blueprint.route("/new_manufacturer")
+# def create_manufacturer():
+#     name = request.form['name']
+#     location = request.form['location']
+#     payment_days = request.form['payment_days']
+#     payment_code = request.form['payment_code']
 
-    manufacturer = Manufacturer(name, location, payment_days, payment_code, id=None)
-    manufacturer_repository.save(manufacturer)
-    return redirect("/manufacturers")
+#     manufacturer = Manufacturer(name, location, payment_days, payment_code, id=None)
+#     manufacturer_repository.save(manufacturer)
+#     return redirect("/manufacturers")
 
--- controllers/products_controller.py
+++ controllers/products_controller.py
@@ -5,8 +5,9 @@ from models.manufacturer import Manufacturer
 from repositories import manufacturer_repository
 from repositories import product_repository
 from models.product import Product
+from models.manufacturer import Manufacturer
 
-product_blueprint = Blueprint("tasks", __name__)
+product_blueprint = Blueprint("products", __name__)
 
 @product_blueprint.route("/products")
 def products():
@@ -15,7 +16,7 @@ def products():
 
 @product_blueprint.route("/new")
 def new_task():
-    # products = product_repository.select_all()
+    products = product_repository.select_all()
     manufacturers = manufacturer_repository.select_all()
 
     return render_template("/new.html", title='New Product', manufacturers = manufacturers)
@@ -49,9 +50,26 @@ def update_product(id):
 
     product = Product(name, description, stock_quantity, buying_cost, selling_cost, manufacturer_id)
     product_repository.update(product)
-    return redirect('/products.html', product = product)
+    return redirect('/products', product = product)
 
 @product_blueprint.route("/manufacturers")
 def manufacturers():
     manufacturers = manufacturer_repository.select_all()
     return render_template("/manufacturers.html", all_manufacturers = manufacturers)
+
+@product_blueprint.route("/new_manufacturer")
+def new_manufacturer():
+    manufacturers = manufacturer_repository.select_all()
+    products = product_repository.select_all()
+    return render_template("/new_manufacturer.html", title="New Manufacturer", manufacturers = manufacturers)
+
+@product_blueprint.route("/new_manufacturer")
+def create_manufacturer():
+    name = request.form.get['name']
+    location = request.form.get['location']
+    payment_days = request.form.get['payment_days']
+    payment_code = request.form.get['payment_code']
+
+    manufacturer = Manufacturer(name, location, payment_days, payment_code, id=None)
+    manufacturer_repository.save(manufacturer)
+    return redirect("/manufacturers")
